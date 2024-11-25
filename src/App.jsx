d61a7eb3cac7f8c5925b50133e246435cd7ef363
Shadan9113 <mdshadanzaheer@gmail.com>
2024-11-25 19:06:26 +0530

    update name compo

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
-import React from 'react'
-import Header from './Component/Header'
-import About from './About'
-import Projects from './Component/Projects'
-import Testimonial from './Component/Testimonial'
-import Contact from './Component/Contact'
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Footer from './Component/Footer'
+import React from "react";
+import Header from "./Component/Header";
+import About from "./About";
+import Projects from "./Component/Projects";
+import Testimonial from "./Component/Testimonial";
+import Contact from "./Component/Contact";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Footer from "./Component/Footer";
+import Name from "./Component/name";
 
 const App = () => {
   return (
-    <div className='w-full overflow-hidden'>
-      <ToastContainer/>
-      <Header/>
-      <About/>
-      <Projects/>
-      <Testimonial/>
-      <Contact/>
-      <Footer/>
+    <div className="w-full overflow-hidden">
+      <Name />
+      <ToastContainer />
+      <Header />
+      <About />
+      <Projects />
+      <Testimonial />
+      <Contact />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/Component/Name.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Name() {
+  return <div>hello world</div>;
+}
+
+export default Name;
